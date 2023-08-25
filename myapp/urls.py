2e4ed70381fd085ec13aeaaa02ccddd1f874006f
Lucky Icban <lddicban@gmail.com>
2023-08-25 21:49:53 +0800

    added logout functionality

--- myapp/urls.py
+++ myapp/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
     path('login/', auth_view.LoginView.as_view(template_name='myapp/login.html'), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='myapp/logout.html'), name='logout'),
 ]

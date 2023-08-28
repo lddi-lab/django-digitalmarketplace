b3ee8b6f89f9d906644c5a93aa5a59e0d00ff6d8
Lucky Icban <lddicban@gmail.com>
2023-08-28 15:05:26 +0800

    added dashboard filter

--- myapp/views.py
+++ myapp/views.py
@@ -51,7 +51,7 @@ def product_delete(request, id):
     return render(request, 'myapp/delete.html', {'product':product})
 
 def dashboard(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(seller=request.user)
     return render(request, 'myapp/dashboard.html', {'products':products })
 
 
